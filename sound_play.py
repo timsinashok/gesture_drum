f574887d0e87c37e152ff70670d22e2c80be2b36
Ashok Timsina <ashoktimsina147181@gmail.com>
2023-07-02 22:40:40 -0400

    setup and manage sound play

---
+++ sound_play.py
@@ -0,0 +1,43 @@
+from playsound import playsound
+
+def play_sound(file_path):
+    playsound(file_path)
+
+# file paths
+bass = '/Users/ashoktimsina/Desktop/gesture_durm/sounds/Bass-Drum-Hit-Level-6c-.mp3'
+drum = '/sounds/Bass-Drum-Hit-Level-6c-.mp3'
+floor = '/sounds/Bass-Drum-Hit-Level-6c-.mp3'
+hi_hat = '/sounds/Bass-Drum-Hit-Level-6c-.mp3'
+snare = '/sounds/Bass-Drum-Hit-Level-6c-.mp3'
+
+prompt = "Enter a number: "
+
+play_sound(bass)
+
+# main loop
+while True:
+    
+    #getting user choice
+    #try:
+    num = input(prompt)
+
+    num = int(num)
+    # except:
+    #     print('Your were supposed to enter a number.')
+    #     continue
+    
+    if (num == 1):
+        play_sound(bass)
+        print('played')
+
+    if num == 2:
+        play_sound(drum)
+
+    if num == 3:
+        play_sound(floor)    
+
+    if num == 4:
+        play_sound(hi_hat)
+    
+    if num == 5:
+        play_sound(snare)
---
+++ triming_sound_files.py
@@ -0,0 +1,26 @@
+from pydub import AudioSegment
+from pydub.silence import split_on_silence
+
+def remove_silence(input_file, output_file, min_silence_len, silence_threshold):
+    # Load the audio file
+    audio = AudioSegment.from_file(input_file)
+
+    # Split the audio based on silence
+    chunks = split_on_silence(audio, min_silence_len=min_silence_len, silence_thresh=silence_threshold)
+
+    # Combine the non-silent chunks
+    combined_audio = AudioSegment.empty()
+    for chunk in chunks:
+        combined_audio += chunk
+
+    # Export the combined audio to a new file
+    combined_audio.export(output_file, format="mp3")
+
+# Example usage:
+input_file = "/Users/ashoktimsina/Desktop/gesture_durm/sounds/Bass-Drum-Hit-Level-6c-.mp3"
+output_file = "/Users/ashoktimsina/Desktop/gesture_durm/sounds/Bass-Drum-Hit-Level-6c-mod.mp3"
+min_silence_len = 100  # Minimum silence duration in milliseconds
+silence_threshold = -30  # Silence threshold in dB
+
+# Call the remove_silence() function with the specified parameters
+remove_silence(input_file, output_file, min_silence_len, silence_threshold)
